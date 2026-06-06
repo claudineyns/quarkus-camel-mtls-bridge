62241bdcfa3e5f0473744826bda1d1bada401d1e
Claudiney Nascimento <contato@claudiney.info>
2026-06-06 00:19:05 -0300

    chore: http2 support

--- src/main/java/com/example/poc/bridge/component/HttpComponentCustomizer.java
+++ src/main/java/com/example/poc/bridge/component/HttpComponentCustomizer.java
@@ -1,13 +1,24 @@
 package com.example.poc.bridge.component;
 
+import io.vertx.core.http.HttpVersion;
 import io.vertx.ext.web.client.WebClientOptions;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
 import org.apache.camel.component.vertx.http.VertxHttpComponent;
 import org.apache.camel.quarkus.main.events.BeforeConfigure;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class HttpComponentCustomizer {
+
+    @Inject
+    @ConfigProperty(name = "bridge.http-client.max-pool-size")
+    int maxPoolSize;
+
+    @Inject
+    @ConfigProperty(name = "bridge.http-client.verify-host")
+    boolean verifyHost;
 
     void configure(@Observes final BeforeConfigure event) {
         final VertxHttpComponent component = event.getCamelContext()
@@ -15,8 +26,10 @@ public class HttpComponentCustomizer {
         component.setWebClientOptions(
                 new WebClientOptions()
                         .setTrustAll(true)
-                        .setVerifyHost(false)
-                        .setMaxPoolSize(25)
+                        .setVerifyHost(verifyHost)
+                        .setMaxPoolSize(maxPoolSize)
+                        .setProtocolVersion(HttpVersion.HTTP_2)
+                        .setUseAlpn(true)
         );
         component.setVertxHttpBinding(new ProxyVertxHttpBinding());
     }
--- src/main/java/com/example/poc/bridge/component/ProxyVertxHttpBinding.java
+++ src/main/java/com/example/poc/bridge/component/ProxyVertxHttpBinding.java
@@ -5,6 +5,8 @@ import io.vertx.ext.web.client.HttpResponse;
 import org.apache.camel.Exchange;
 import org.apache.camel.component.vertx.http.DefaultVertxHttpBinding;
 import org.apache.camel.spi.HeaderFilterStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Clears inbound request headers from the exchange before populating it with
@@ -13,10 +15,13 @@ import org.apache.camel.spi.HeaderFilterStrategy;
  */
 public class ProxyVertxHttpBinding extends DefaultVertxHttpBinding {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ProxyVertxHttpBinding.class);
+
     @Override
     public void populateResponseHeaders(Exchange exchange,
                                         HttpResponse<Buffer> response,
                                         HeaderFilterStrategy headerFilterStrategy) {
+        LOG.trace("outbound protocol: {} → backend HTTP {}", response.version(), response.statusCode());
         exchange.getMessage().removeHeaders("*");
         super.populateResponseHeaders(exchange, response, headerFilterStrategy);
     }
